6aaf72b5dba422018762c6b4fc821f9010f4ce86
Sergio <sergio-guitar@yandex.ru>
2023-06-22 14:52:49 +0300

    Task2

---
+++ Java_tasks/Sem1/Task2.java
@@ -0,0 +1,13 @@
+package Java_tasks.Sem1;
+/**
+ * Task 2
+ * Вывести все простые числа от 1 до 1000 (не включая 1000)
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        int first = 1;
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
